ece2cc4947dcc5bd38a0e855d774530da0615268
besahilll <99262172+besahilll@users.noreply.github.com>
2022-06-05 00:12:42 +0530

    Create area_of_rect(user).c
    This code is for calculating area of rectangle, dimensions input by the user.

---
+++ area_of_rect(user).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+    int main(){
+    int l,b;
+    printf("Enter the length & breadth of rectangle");
+    scanf("%d%d",&l,&b);
+    printf("Area of rectangle is %d", l*b);
+
+return 0;
+}
