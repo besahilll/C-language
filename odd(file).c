59982031936f76e3a2a7c77ad7a0ba28c95e219f
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2023-11-29 23:59:50 +0530

    Create odd(file).c

---
+++ odd(file).c
@@ -0,0 +1,17 @@
+//WAP to write all the odd numbers from 1 to n in a file.
+#include<stdio.h>
+
+    int main(){
+    FILE *fptr;
+    fptr=fopen("odd.txt","w");
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        if(i%2!=0){
+            fprintf(fptr,"%d\n",i);
+        }
+    }
+    fclose(fptr);
+return 0;
+}
